d434ca9a4a066d47ceab5773c5cea95c1127c901
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-02-26 16:17:02 -0300

    feat: add validations dependency and apply to schedule dtos

--- src/main/java/com/yuriolivs/notification/shared/domain/schedule/dto/SchedulePayloadDTO.java
+++ src/main/java/com/yuriolivs/notification/shared/domain/schedule/dto/SchedulePayloadDTO.java
@@ -1,8 +1,14 @@
 package com.yuriolivs.notification.shared.domain.schedule.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
 public record SchedulePayloadDTO(
+        @NotNull
         UUID id,
+
+        @NotBlank
         String payload
 ) {}
--- src/main/java/com/yuriolivs/notification/shared/domain/schedule/dto/ScheduledPayloadResponseDTO.java
+++ src/main/java/com/yuriolivs/notification/shared/domain/schedule/dto/ScheduledPayloadResponseDTO.java
@@ -1,7 +1,10 @@
 package com.yuriolivs.notification.shared.domain.schedule.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record ScheduledPayloadResponseDTO(
+        @NotNull
         List<SchedulePayloadDTO> list
 ) {}
