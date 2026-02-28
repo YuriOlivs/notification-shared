736d144e39a70a030aab20dccec3b0bb5bcc68a3
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-02-28 14:09:26 -0300

    feat: add notification result

---
+++ src/main/java/com/yuriolivs/notification/shared/domain/notification/NotificationResult.java
@@ -0,0 +1,54 @@
+package com.yuriolivs.notification.shared.domain.notification;
+
+import java.util.UUID;
+
+public class NotificationResult {
+    private UUID id;
+    private UUID scheduleId;
+    private boolean successful;
+    private String message;
+
+    public NotificationResult(
+            UUID id,
+            UUID scheduleId,
+            boolean successful,
+            String message
+    ) {
+        this.id = id;
+        this.scheduleId = scheduleId;
+        this.successful = successful;
+        this.message = message;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(UUID scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public boolean success() {
+        return successful;
+    }
+
+    public void setSuccess(boolean wasSucessful) {
+        this.successful = wasSucessful;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
